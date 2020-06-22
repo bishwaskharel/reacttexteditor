3b772d02d51d0a974149ce4487a94287120ab61f
Bishwas Kharel <bishukharel98@gmail.com>
2020-06-22 20:43:21 +0545

    Updates

--- precache-manifest.a96ed91c2fcbdb4ab4b919421b5acbba.js
+++ precache-manifest.a96ed91c2fcbdb4ab4b919421b5acbba.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24122d4bebf4e5960381883fa89ad9e3",
+    "revision": "9e34e8055889666db56e053bcaa8d17d",
     "url": "/reacttexteditor/index.html"
   },
   {
-    "revision": "b6bcd3a5c13efbab906a",
-    "url": "/reacttexteditor/static/css/main.02a061bd.chunk.css"
+    "revision": "f8c5fada7b62bfdcbec7",
+    "url": "/reacttexteditor/static/css/main.348b6644.chunk.css"
   },
   {
     "revision": "d28a0e22ff4f76de1c49",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reacttexteditor/static/js/2.183f1cae.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b6bcd3a5c13efbab906a",
-    "url": "/reacttexteditor/static/js/main.68d9e91a.chunk.js"
+    "revision": "f8c5fada7b62bfdcbec7",
+    "url": "/reacttexteditor/static/js/main.bda39a4b.chunk.js"
   },
   {
     "revision": "44d00857af7e4849ecd7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reacttexteditor/precache-manifest.c6e78b19141826e921da2d47d59565da.js"
+  "/reacttexteditor/precache-manifest.a96ed91c2fcbdb4ab4b919421b5acbba.js"
 );
 
 self.addEventListener('message', (event) => {
