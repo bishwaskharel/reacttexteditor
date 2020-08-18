ddb02242a66e64344f6f0f466bdf0228ea00e507
Bishwas Kharel <bishukharel98@gmail.com>
2020-08-18 09:43:52 +0545

    auto number generator

--- src/Forms.js
+++ src/Forms.js
@@ -3,26 +3,39 @@ import React, { useState } from "react";
 const Forms = () => {
   const [name, setName] = useState("");
   const [fullName, setFullName] = useState("");
-
+  const [password, setPassword] = useState("");
   const inputEvent = (event) => {
     setName(event.target.value);
   };
 
   const onChange = () => {
     let a = name;
+    let b = password;
+    let c = a + b;
     setFullName(a);
+    setPassword(c);
   };
   return (
     <>
       <h1>Hello {fullName}</h1>
       <div style={{ textAlign: "center" }}>
-        <input
-          type="text"
-          placeholder="Enter Name"
-          value={name}
-          onChange={inputEvent}
-        />
-        <button onClick={onChange}>Sumbit</button>
+        <form onClick={onChange}>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={inputEvent}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            defaultValue={password}
+            onchange={inputEvent}
+          />
+          <div>
+            <button type="submit">Sumbit</button>
+          </div>
+        </form>
       </div>
     </>
   );
--- src/InNum.js
+++ src/InNum.js
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 
 const InNum = () => {
-
   const [count, setCount] = useState(0);
 
   const IncNum2 = () => {
     setCount(count + 1);
   };
 
-  return(
-      <>
-    <div className="text-center">
-    <h3>{count}</h3>
-    <button type="button" onClick={IncNum2} className="btn btn-success ">
-      Increase Number
-    </button>
-  </div>
-  </>
-  )
-  }
+  const [count2, setCount2] = useState(0);
+  const upNum = () => {
+    let abc = count2 + 1
+    setCount2(abc);
+    console.log("2")
+    console.log("3")
+  };
 
-  export default InNum;
-  
+  setTimeout(upNum, 1000);
+  setTimeout(console.log("1"),1000)
+
+  return (
+    <>
+      <div className="text-center">
+        <h3>{count}</h3>
+        <button type="button" onClick={IncNum2} className="btn btn-success ">
+          Increase Number
+        </button>
+        <h1>{count2}</h1>
+      </div>
+    </>
+  );
+};
+
+export default InNum;
